3e197002d473121e174b73fa91df490b9e050ea6
KjetilOverby <kjeoever@msn.com>
2024-02-03 18:28:30 +0100

    service stats

---
+++ src/components/reusable/BSFTable.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const BSFTable = () => {
+  return (
+    <div>
+      <table className="table table-xs ml-5 w-full border border-primary bg-base-100 text-neutral shadow-xl">
+        <thead>
+          <tr className="border border-primary">
+            <th className="text-lg italic text-neutral">Kode</th>
+            <th className="text-lg italic text-neutral">Beskrivelse</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border border-primary">
+            <th>BFS423</th>
+            <th>EKSTRA RETTING BÅND</th>
+          </tr>
+          <tr className="border border-primary">
+            <th>BFS426</th>
+            <th>BUNNSTUK BÅND</th>
+          </tr>
+          <tr className="border border-primary">
+            <th>BFS427</th>
+            <th>RETTING-STREKKING-SLIPING METER</th>
+          </tr>
+          <tr className="border border-primary">
+            <th>BFS429</th>
+            <th>STELL.FERDIG SLIP OG RETT f.o.m 100mm bredde TANN</th>
+          </tr>
+          <tr className="border border-primary">
+            <th>BSF438</th>
+            <th>REP.SVEIST STELLIT TANN</th>
+          </tr>
+          <tr>
+            <th>BFS442</th>
+            <th>SLIPESERVICE AV REP.TENNER METER</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BSFTable;
---
+++ src/components/statistikk/serviceStats/Handling.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Handling = () => {
+  return (
+    <div className="mt-5 bg-white">
+      <h1>Service</h1>
+    </div>
+  );
+};
+
+export default Handling;
